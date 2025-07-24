f99be9d803ad0ded18e659dfd7eb68ce961d886f
Eric3337 <ezhang3337@gmail.com>
2025-07-23 21:52:27 -0600

    Refactor and Create all components for list of categories

--- voicelist/src/app/page.tsx
+++ voicelist/src/app/page.tsx
@@ -9,46 +9,6 @@ const SORTDOWNARROW = sortDownArrow.src;
 const SORTUPARROW = sortUpArrow.src;
 const DELETEICON = image.src;
 
-export default function Home() {
-  console.log(image);
-  return (
-    <div id ="voicelist-home">
-      <h1>VoiceList</h1>
-      <CategoryList />
-      <SortDownArrow />
-      <SortUpArrow />
-    </div>
-  );
-}
-
-function CategoryList() {
-  return (
-    <div id="category-list">
-      <h2 id="category-list-title">Categories</h2>
-      <CategorySearchBar />
-      <CategoryRow />
-      <CategoryRow />
-    </div>
-  );
-}
-
-function CategorySearchBar() {
-  return (
-    <form id="category-search-bar">
-      <input type="text" placeholder="Search..." />
-    </form>
-  );
-}
-
-function CategoryRow() {
-  return (
-    <div className="category-row">
-      <h3>[Category Name]</h3>
-      <DeleteIcon />
-    </div>
-  );
-}
-
 function DeleteIcon() {
   return (
     <div>
@@ -59,14 +19,112 @@ function DeleteIcon() {
   );
 }
 
-function SortDownArrow() {
+function CategoryRow() {
   return (
-    <img src={SORTDOWNARROW} alt="Sort Down" />
-  )
+    <tr>
+      <td>[Category Name]</td>
+      <td>[Date of Creation]</td>
+      <td><DeleteIcon /></td>
+    </tr>
+  );
 }
 
 function SortUpArrow() {
   return (
-    <img src={SORTUPARROW} alt="Sort Up" />
+    <div className="sort-arrow">
+      <img src={SORTUPARROW} alt="Sort Up" />
+    </div>
   )
+}
+
+function SortDownArrow() {
+  return (
+    <div className="sort-arrow">
+      <img src={SORTDOWNARROW} alt="Sort Down" />
+    </div>
+  )
+}
+
+function CreationDateFilter() {
+  return (
+    <div id="creation-date-filter">
+      <h3>Creation Date</h3>
+      <SortDownArrow />
+    </div>
+  )
+}
+
+function NameFilter() {
+  return (
+    <div id="name-filter">
+      <h3>Name</h3>
+      <SortDownArrow />
+    </div>
+  )
+}
+
+function CategoryTable() {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th><NameFilter /></th>
+          <th><CreationDateFilter /></th>
+        </tr>
+      </thead>
+      <tbody><CategoryRow /></tbody>
+    </table>
+  )
+}
+
+function CategorySearchBar() {
+  return (
+    <form id="category-search-bar">
+      <input type="text" placeholder="Search..." />
+    </form>
+  );
+}
+
+function RecordButton() {
+  return (
+    <a href="#">
+      <button id="record-button" style={{ backgroundColor: "red"}}>
+        <label>
+          Record
+        </label>
+      </button>
+    </a>
+  )
+}
+
+function ItemList() {
+  return (
+    <div id="item-list" style={{ backgroundColor: "pink"}}>
+      testing
+    </div>
+  )
+} 
+
+function CategoryList() {
+  return (
+    <div id="category-list" style={{ backgroundColor: "lightblue"}}>
+      <h2 id="category-list-title">Categories</h2>
+      <CategorySearchBar />
+      <CategoryTable />
+    </div>
+  );
+}
+
+export default function Home() {
+  console.log(image);
+  return (
+    <div id ="voicelist-home">
+      <h1>VoiceList</h1>
+      <div className="main-content">
+        <CategoryList />
+        <ItemList />
+      </div>
+      <RecordButton />
+    </div>
+  );
 }
